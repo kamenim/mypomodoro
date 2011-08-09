c3aa9c29b46220942b0de092b9762d4a22383735
phil.karoo <phil.karoo@gmail.com>
2011-08-09 12:39:48 +0000

    ToDo List: + added Mute button

---
+++ src/main/java/org/mypomodoro/buttons/MuteButton.java
@@ -0,0 +1,42 @@
+package org.mypomodoro.buttons;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import org.mypomodoro.Main;
+import org.mypomodoro.gui.todo.Pomodoro;
+
+public class MuteButton extends JButton {
+
+    private final ImageIcon muteIcon = new ImageIcon(Main.class.getResource("/images/mute.png"));
+    private final ImageIcon soundIcon = new ImageIcon(Main.class.getResource("/images/sound.png"));
+    private boolean isMuteIcon = true;
+
+    public MuteButton(final Pomodoro pomodoro) {
+        setMuteIcon();
+        addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (isMuteIcon) {
+                    setSoundIcon();
+                    pomodoro.mute();
+                    isMuteIcon = false;
+                } else {
+                    setMuteIcon();
+                    pomodoro.unmute();
+                    isMuteIcon = true;
+                }
+            }
+        });
+    }
+
+    private void setSoundIcon() {
+        setIcon(soundIcon);
+    }
+
+    private void setMuteIcon() {
+        setIcon(muteIcon);
+    }
+}
