a4c69d0665a97c5bf32281f63e01bd418e1a1100
philkaroo <philkaroo@f81ebdc8-a4cc-4bc0-871f-e29acdb6ada4>
2014-02-10 10:43:54 +0000

    some cleanup

--- trunk/release_to_download.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# vim: set sw=4 sts=4 et foldmethod=indent :
-#You will need python to run this
-LABELS="Feature"
-SUMMARY="myPomodoro Executable Jar Zip"
-PROJECT_NAME="mypomodoro"
-python googlecode_upload.py -s "${SUMMARY}" -p "${PROJECT_NAME}" -l "${LABELS}" $@
