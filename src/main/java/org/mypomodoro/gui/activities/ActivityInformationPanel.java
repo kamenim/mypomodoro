d651b2a22f813b4b37bbce19de94d2cdfd0b7e11
philkaroo <philkaroo@f81ebdc8-a4cc-4bc0-871f-e29acdb6ada4>
2014-02-11 15:50:00 +0000

    Renamed user guide folder

--- src/main/java/org/mypomodoro/gui/activities/ActivityInformationPanel.java
+++ src/main/java/org/mypomodoro/gui/activities/ActivityInformationPanel.java
@@ -14,21 +14,21 @@ import org.mypomodoro.util.Labels;
  * @author Phil Karoo
  */
 public class ActivityInformationPanel extends JPanel implements ActivityInformation {
-    
+
     protected final JTextArea informationArea = new JTextArea();
     protected final ArrayList<String> textArray = new ArrayList();
     protected final StringBuffer info = new StringBuffer();
-    
+
     @Override
-    public void selectInfo(Activity activity) {        
+    public void selectInfo(Activity activity) {
         textArray.add(Labels.getString("Common.Date") + ": "
-            + (activity.isUnplanned()?"U [":"")
-            + DateUtil.getFormatedDate(activity.getDate())    
-            + (activity.isUnplanned()?"]":""));
+                + (activity.isUnplanned() ? "U [" : "")
+                + DateUtil.getFormatedDate(activity.getDate())
+                + (activity.isUnplanned() ? "]" : ""));
         //textArray.add(" " + DateUtil.getFormatedTime(activity.getDate()));
         textArray.add("\n" + Labels.getString("Common.Title") + ": " + activity.getName()
                 + "\n" + Labels.getString("Common.Estimated pomodoros") + ": " + activity.getEstimatedPoms()
-                + (activity.getOverestimatedPoms() > 0?" + " + activity.getOverestimatedPoms():""));
+                + (activity.getOverestimatedPoms() > 0 ? " + " + activity.getOverestimatedPoms() : ""));
         textArray.add("\n" + Labels.getString("ReportListPanel.Real Pomodoros") + ": " + activity.getActualPoms());
         textArray.add("\n" + Labels.getString("ReportListPanel.Diff I") + ": " + (activity.getActualPoms() - activity.getEstimatedPoms()));
         textArray.add("\n" + Labels.getString("ReportListPanel.Diff II") + ": " + (activity.getOverestimatedPoms() > 0 ? activity.getActualPoms() - activity.getEstimatedPoms() - activity.getOverestimatedPoms() : ""));
@@ -37,21 +37,21 @@ public class ActivityInformationPanel extends JPanel implements ActivityInformat
         textArray.add("\n" + Labels.getString("Common.Type") + ": " + (activity.getType().isEmpty() ? "-" : activity.getType()));
         textArray.add("\n" + Labels.getString("Common.Author") + ": " + (activity.getAuthor().isEmpty() ? "-" : activity.getAuthor()));
         textArray.add("\n" + Labels.getString("Common.Place") + ": " + (activity.getPlace().isEmpty() ? "-" : activity.getPlace()));
-        textArray.add("\n" + Labels.getString("Common.Description") + ": " + (activity.getDescription().isEmpty() ? "-" : activity.getDescription()));        
+        textArray.add("\n" + Labels.getString("Common.Description") + ": " + (activity.getDescription().isEmpty() ? "-" : activity.getDescription()));
     }
-    
+
     @Override
     public void showInfo() {
-        for(String line : textArray) {
+        for (String line : textArray) {
             info.append(line);
         }
         informationArea.setText(info.toString());
         // disable auto scrolling
         informationArea.setCaretPosition(0);
     }
-    
+
     @Override
     public void clearInfo() {
         informationArea.setText("");
-    }    
+    }
 }
