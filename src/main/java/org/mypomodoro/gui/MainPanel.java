27f93ed1d0ce9dacfa9476ef0611d1cc114a0895
philkaroo <philkaroo@f81ebdc8-a4cc-4bc0-871f-e29acdb6ada4>
2015-02-10 05:30:47 +0000

    changed version 3.2.0 --> 3.3.0

--- src/main/java/org/mypomodoro/gui/MainPanel.java
+++ src/main/java/org/mypomodoro/gui/MainPanel.java
@@ -55,7 +55,7 @@ public final class MainPanel extends JFrame {
     public static final int FRAME_WIDTH = 780;
     public static final int FRAME_HEIGHT = 580;
     public static TrayIcon trayIcon;
-    public static final String MYPOMODORO_VERSION = "3.2.0";
+    public static final String MYPOMODORO_VERSION = "3.3.0";
     private final MenuBar menuBar = new MenuBar(this);
     private final IconBar iconBar = new IconBar(this);
     private final WindowPanel windowPanel = new WindowPanel(iconBar, this);
